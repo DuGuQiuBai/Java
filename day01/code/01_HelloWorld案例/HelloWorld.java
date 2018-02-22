8362bf5a34e9e49ed3f696af4ef7aaf7b051bb8e
harshitsahu1 <harshitvns57@gmail.com>
2018-02-23 01:18:57 +0530

    Update HelloWorld.java

--- day01/code/01_HelloWorld案例/HelloWorld.java
+++ day01/code/01_HelloWorld案例/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("�Ұ�����ϼ");
+	public static void main(String args[]) {
+		System.out.println("ÎÒ°®ÁÖÇàÏ¼");
 	}
 }
