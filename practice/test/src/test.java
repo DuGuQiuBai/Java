e67af69bc64a9b3a9856c2ac1dab0174eb5f9d53
TigerNiu <2019302050022@whu.edu.cn>
2022-02-28 15:09:21 +0800

    ky

--- practice/test/src/test.java
+++ practice/test/src/test.java
@@ -1,5 +1,6 @@
-public class App {
+public class test {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.print('我');
     }
 }
