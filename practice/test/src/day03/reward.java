08e816a86c788f2dd2c569e05a9257327a00bd0a
TigerNiu <2019302050022@whu.edu.cn>
2022-03-05 19:45:26 +0800

    day03

--- practice/test/src/day03/reward.java
+++ practice/test/src/day03/reward.java
@@ -1,36 +1,48 @@
 package day03;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class reward {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int scole = sc.nextInt();
-        if (scole < 0 || scole >100) {
-            System.out.println("错误数据，请重新输入");
-            int scole2 = sc.nextInt();
-            if (scole2 >= 95 && scole2 <= 100) {
-                System.out.println("奖励100元");
-            } else if (scole2 >= 85 && scole2 < 95) {
-                System.out.println("奖励90元");
-            } else if (scole2 >= 70 && scole2 < 85) {
-                System.out.println("奖励50元");
-            } else if (scole2 >= 60 && scole2 < 70) {
-                System.out.println("奖励一个大嘴巴子");
-            } else {
-                int k = 100-scole2;
-                System.out.println("奖励" + k + "个大嘴巴子");
+        Scanner range = new Scanner(System.in);
+        System.out.print("输入卷面满分:");
+        String num;
+        if (range.hasNextInt()) {
+            num = range.next();
+            int total = Integer.parseInt(num);
+            if (total < 0) {
+                System.out.println("cnmd输错啦！！！！！！！！");
+                range.close();
+                return;
             }
-        } else if (scole >= 95 && scole <= 100) {
-            System.out.println("奖励100元");
-        } else if (scole >= 85 && scole < 95) {
-            System.out.println("奖励90元");
-        } else if (scole >= 70 && scole < 85) {
-            System.out.println("奖励50元");
-        } else if (scole >= 60 && scole < 70) {
-            System.out.println("奖励一个大嘴巴子");
+            System.out.print("输入你的得分");
+            Scanner sc = new Scanner(System.in);
+            String num2;
+            if (sc.hasNextInt()) {
+                num2 = sc.next();
+                int scole = Integer.parseInt(num2);
+                if (scole >= total*0.9 && scole <= total) {
+                    System.out.println("奖励100元");
+                } else if (scole >= total*0.8 && scole < total*0.9) {
+                    System.out.println("奖励90元");
+                } else if (scole >= total*0.7 && scole < total*0.8) {
+                    System.out.println("奖励50元");
+                } else if (scole >= total*0.6 && scole < total*0.7) {
+                    System.out.println("奖励一个大嘴巴子");
+                } else if (scole < total*0.6 && scole >= 0){
+                    int k = (int)total - scole;
+                    System.out.println("奖励" + k + "个大嘴巴子");
+                } else {
+                    System.out.println("成绩不在范围内");
+                }
+                sc.close();
+            } else {
+                System.out.println("cnmd输错啦！！！！！！！！");
+            }
+
         } else {
-            int k = 100-scole;
-            System.out.println("奖励" + k + "个大嘴巴子");
+            System.out.println("cnmd输错啦！！！！！！！！");
         }
-        sc.close();
+        range.close();
     }
 }
