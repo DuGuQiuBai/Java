a95688395e7a2e9a110ef4b176b4c4e747a0c3e5
TigerNiu <2019302050022@whu.edu.cn>
2022-03-05 22:17:20 +0800

    day03

---
+++ practice/test/src/day03/and.java
@@ -0,0 +1,7 @@
+package day03;
+import java.util.Scanner;
+public class and {
+    public static void main(String[] args) {
+        System.out.print("s");
+    }
+}
--- practice/test/src/day03/reward.java
+++ practice/test/src/day03/reward.java
@@ -1,26 +1,26 @@
 package day03;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 public class reward {
     public static void main(String[] args) {
         Scanner range = new Scanner(System.in);
         System.out.print("输入卷面满分:");
         String num;
-        if (range.hasNextInt()) {
+        if (!range.hasNextInt() && range.hasNextDouble()) {
+            System.out.println("tnnd谁设计的卷面满分是小数的？？！！！给老子去SPA！！！！");
+        } else if (range.hasNextDouble()) {
             num = range.next();
-            int total = Integer.parseInt(num);
+            double total = Double.parseDouble(num);
             if (total < 0) {
                 System.out.println("cnmd输错啦！！！！！！！！");
                 range.close();
                 return;
             }
-            System.out.print("输入你的得分");
+            System.out.print("输入你的得分:");
             Scanner sc = new Scanner(System.in);
             String num2;
-            if (sc.hasNextInt()) {
+            if (sc.hasNextDouble()) {
                 num2 = sc.next();
-                int scole = Integer.parseInt(num2);
+                double scole = Double.parseDouble(num2);
                 if (scole >= total*0.9 && scole <= total) {
                     System.out.println("奖励100元");
                 } else if (scole >= total*0.8 && scole < total*0.9) {
@@ -30,18 +30,19 @@ public class reward {
                 } else if (scole >= total*0.6 && scole < total*0.7) {
                     System.out.println("奖励一个大嘴巴子");
                 } else if (scole < total*0.6 && scole >= 0){
-                    int k = (int)total - scole;
+                    int k = (int)(total - scole);
                     System.out.println("奖励" + k + "个大嘴巴子");
+                } else if (scole > total){
+                    System.out.println("你个小子是不是改分了？？满分是多少都不知道？？？欠揍是吧");
                 } else {
-                    System.out.println("成绩不在范围内");
+                    System.out.println("少的分被你吃了？？你个饭桶");
                 }
                 sc.close();
             } else {
-                System.out.println("cnmd输错啦！！！！！！！！");
+                System.out.println("cnmd输错啦！！！！");
             }
-
         } else {
-            System.out.println("cnmd输错啦！！！！！！！！");
+            System.out.println("你他奶奶滴输错啦！！！你家卷子满分是符号和字母？？？");
         }
         range.close();
     }
