65b5868813cf410e7750a11834de104ab91118d6
TigerNiu <2019302050022@whu.edu.cn>
2022-03-10 18:32:02 +0800

    day06

--- practice/test/src/day06/Student.java
+++ practice/test/src/day06/Student.java
@@ -3,6 +3,21 @@ package day06;
 public class Student {
     String name;
     int age;
+    public void setAge(int a) {
+        if (a < 0 || a > 120) {
+            System.out.println("数据有误");
+        } else {
+            age = a;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void show() {
+        System.out.println(name + ", " + age);
+    }
 
     public void study() {
         System.out.println("好好学习");
--- practice/test/src/day06/studentdemo.java
+++ practice/test/src/day06/studentdemo.java
@@ -3,11 +3,13 @@ package day06;
 public class studentdemo {
     public static void main(String[] args) {
         Student s = new Student();
-        s.age = 30;
+        s.setAge(30);
         s.name = "daqing";
+        s.show();
         s.study();
         s.doHomework();
-        System.out.println(s.age);
-        System.out.println(s.name);
+        System.out.println(s.getAge() + "-----" + s.name);
+        // System.out.println(s.age);
+        // System.out.println(s.name);
     }
 }
