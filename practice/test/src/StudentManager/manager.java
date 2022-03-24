9e5e12f9cc25df0045ee017c919e9926d5c91562
TigerNiu <2019302050022@whu.edu.cn>
2022-03-24 23:30:15 +0800

    day08

--- practice/test/src/StudentManager/manager.java
+++ practice/test/src/StudentManager/manager.java
@@ -3,49 +3,58 @@ package StudentManager;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class manager {
     static Scanner sc = new Scanner(System.in);
+
+    public void close() {
+        sc.close();
+    }
+
     public static void main(String[] args) {
         ArrayList<Student> array = new ArrayList<Student>();
-        //Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.println("------Welcome to the StudentManager System------");
-            System.out.println("1 Add Students");
-            System.out.println("2 Delete Students");
-            System.out.println("3 Change Info");
-            System.out.println("4 Check All");
-            System.out.println("5 Quit");
-            System.out.println("Please insert your choice: ");
-
+            PrintTable();
 
             switch (sc.nextLine()) {
                 case "1":
-                addStudent(array);
-                break;
+                    AddStudent(array);
+                    break;
                 case "2":
-                System.out.println("Delete Students");
-                break;
+                    DeleteStudent(array);
+                    ;
+                    break;
                 case "3":
-                System.out.println("Change Info");
-                break;
+                    System.out.println("Change Info");
+                    break;
                 case "4":
-                System.out.println("Check All");
-                break;
+                    CheckAll(array);
+                    break;
                 case "5":
-                System.out.println("Thanks for Using");
-                sc.close();
-                System.exit(0);
+                    System.out.println("Thanks for Using");
+                    sc.close();
+                    return;
                 default:
-                System.out.println("Please choose the right one");
-                break;
+                    System.out.println("Please choose the right one");
+                    break;
             }
         }
+
     }
-    
+    // 打印表头
+    public static void PrintTable() {
+        System.out.println("------Welcome to the StudentManager System------");
+        System.out.println("1 Add Students");
+        System.out.println("2 Delete Students");
+        System.out.println("3 Change Info");
+        System.out.println("4 Check All");
+        System.out.println("5 Quit");
+        System.out.println("Please input your choice: ");
+    }
+
     // 添加学生
-    public static void addStudent(ArrayList<Student> array) {
-        //Scanner sc1 = new Scanner(System.in);
+    public static void AddStudent(ArrayList<Student> array) {
+        // Scanner sc1 = new Scanner(System.in);
 
         System.out.print("Please input id:");
         String sid = sc.nextLine();
@@ -66,5 +75,35 @@ public class manager {
         // 添加学生到集合里
         array.add(s);
         System.out.println("Add Succeed!");
+    }
+
+    // 删除学生
+    public static void DeleteStudent(ArrayList<Student> array) {
+        System.out.print("Please input student's id:");
+        String sid = sc.nextLine();
+
+        for (int i = 0; i < array.size(); i++) {
+            Student s = array.get(i);
+            if (s.getSid().equals(sid)) {
+                array.remove(i);
+                break;
+            }
+        }
+        System.out.println("Delete Succeed!");
+    }
+
+    // 查看学生
+    public static void CheckAll(ArrayList<Student> array) {
+        if (array.size() == 0) {
+            System.out.println("No data, please input students' info!");
+            return;
+        }
+
+        // 表头信息
+        System.out.println("id\t\tname\t\tage\taddress");
+        for (int i = 0; i < array.size(); i++) {
+            Student s = array.get(i);
+            System.out.println(s.getSid() + "\t\t" + s.getName() + "\t\t" + s.getAge() + "岁\t" + s.getAddress());
+        }
     }
 }
